4e2be6a7a2fd6176df4e0ebccf74927418cd5e89
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-31 02:51:53 +0530

    Fix DsStepConnector.Overrides

--- src/Components/DsStepConnector/DsStepConnector.Overrides.ts
+++ src/Components/DsStepConnector/DsStepConnector.Overrides.ts
@@ -1,1 +1,15 @@
-export const DsStepConnectorOverrides = {}
+export const DsStepConnectorOverrides = {
+  MuiStepConnector: {
+    styleOverrides: {
+      line: {
+        borderLeftWidth: '2px',
+        borderLeftStyle: 'solid',
+        borderLeftColor: 'var(--ds-color-strokeDefault)',
+        minHeight: 'var(--ds-rules-stepperConnectorMinHeight)'
+      },
+      vertical: {
+        marginLeft: '27px'
+      }
+    }
+  }
+}
