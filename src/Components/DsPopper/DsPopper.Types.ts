2ce112c450e65c418b8c18292aa3d1275c8ef54f
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-31 01:46:06 +0530

    Fix DsPopper Types

--- src/Components/DsPopper/DsPopper.Types.ts
+++ src/Components/DsPopper/DsPopper.Types.ts
@@ -1,3 +1,5 @@
-export { PopperProps as DsPopperProps } from '@mui/material/Popper'
+import { PopperProps } from '@mui/material'
+
+export interface DsPopperProps extends PopperProps {}
 
 export const DsPopperDefaultProps = {}
