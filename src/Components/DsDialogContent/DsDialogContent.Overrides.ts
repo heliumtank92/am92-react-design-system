769e41ffd65e96f63c2be7d13f290d7bc345b54d
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-31 02:38:50 +0530

    Fix DsDialogContent.Overrides

--- src/Components/DsDialogContent/DsDialogContent.Overrides.ts
+++ src/Components/DsDialogContent/DsDialogContent.Overrides.ts
@@ -1,1 +1,25 @@
-export const DsDialogContentOverrides = {}
+export const DsDialogContentOverrides = {
+  MuiDialogContent: {
+    variants: [
+      {
+        props: { textOnly: true },
+        style: {
+          fontWeight: 'var(--ds-typo-bodyRegularMedium-fontWeight)',
+          fontSize: 'var(--ds-typo-bodyRegularMedium-fontSize)',
+          lineHeight: 'var(--ds-typo-bodyRegularMedium-lineHeight)',
+          letterSpacing: 'var(--ds-typo-bodyRegularMedium-letterSpacing)',
+          paddingBottom: 'var(--ds-spacing-glacial)',
+          textAlign: 'center'
+        }
+      }
+    ],
+    styleOverrides: {
+      root: {
+        color: 'var(--ds-color-typoTertiary)',
+        paddingLeft: 'var(--ds-spacing-bitterCold)',
+        paddingRight: 'var(--ds-spacing-bitterCold)',
+        paddingBottom: 'var(--ds-spacing-mild)'
+      }
+    }
+  }
+}
