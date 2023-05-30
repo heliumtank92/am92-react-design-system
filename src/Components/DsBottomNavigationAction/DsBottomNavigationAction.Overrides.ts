690d68f8fe9abeb7cd9ddc8a6943a78a91016a13
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-31 02:32:51 +0530

    Fix DsBottomNavigationAction.Overrides

--- src/Components/DsBottomNavigationAction/DsBottomNavigationAction.Overrides.ts
+++ src/Components/DsBottomNavigationAction/DsBottomNavigationAction.Overrides.ts
@@ -1,1 +1,38 @@
-export const DsBottomNavigationActionOverrides = {}
+import { DsBottomNavigationActionDefaultProps } from './DsBottomNavigationAction.Types'
+
+export const DsBottomNavigationActionOverrides = {
+  MuiBottomNavigationAction: {
+    defaultProps: DsBottomNavigationActionDefaultProps,
+    styleOverrides: {
+      root: {
+        marginLeft: 'var(--ds-spacing-quickFreeze)',
+        marginRight: 'var(--ds-spacing-quickFreeze)',
+        borderTopWidth: '2px',
+        borderTopStyle: 'solid',
+        borderTopColor: 'transparent',
+        padding: 'var(--ds-spacing-zero)',
+        justifyContent: 'flex-start',
+        color: 'var(--ds-color-iconDisabled)',
+        '> .MuiIcon-root': {
+          paddingTop: 'var(--ds-spacing-glacial)'
+        },
+        '&.Mui-selected': {
+          borderTopColor: 'var(--ds-color-actionSecondary)',
+          color: 'var(--ds-color-actionSecondary)'
+        }
+      },
+      label: {
+        fontWeight: 'var(--ds-typo-bodyRegularSmall-fontWeight)',
+        fontSize: 'var(--ds-typo-bodyRegularSmall-fontSize)',
+        lineHeight: 'var(--ds-typo-bodyRegularSmall-lineHeight)',
+        letterSpacing: 'var(--ds-typo-bodyRegularSmall-letterSpacing)',
+        color: 'var(--ds-color-typoDisabled)',
+        paddingBottom: 'var(--ds-spacing-quickFreeze)',
+        '&.Mui-selected': {
+          fontSize: 'var(--ds-typo-bodyRegularSmall-fontSize)',
+          color: 'var(--ds-color-actionSecondary)'
+        }
+      }
+    }
+  }
+}
