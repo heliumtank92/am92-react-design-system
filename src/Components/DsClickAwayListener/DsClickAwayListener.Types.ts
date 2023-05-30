324fe48a33a56beeae20ce1c167094bc844bbe5a
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-30 19:36:33 +0530

    Fix DsClickAwayListener.Types

--- src/Components/DsClickAwayListener/DsClickAwayListener.Types.ts
+++ src/Components/DsClickAwayListener/DsClickAwayListener.Types.ts
@@ -2,4 +2,4 @@ import { ClickAwayListenerProps } from '@mui/material'
 
 export interface DsClickAwayListenerProps extends ClickAwayListenerProps {}
 
-export const DsClickAwayListenerDefaultProps: DsClickAwayListenerProps = {}
+export const DsClickAwayListenerDefaultProps = {}
