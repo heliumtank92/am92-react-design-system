468dac726ea2de5f49cbf6aae7b02f5b8b537beb
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-05-31 14:12:59 +0530

    Complete DsAutocomplete

--- src/Components/DsAutocomplete/DsAutocomplete.Types.ts
+++ src/Components/DsAutocomplete/DsAutocomplete.Types.ts
@@ -1,24 +1,10 @@
 import { AutocompleteProps } from '@mui/material'
+
 export interface DsAutocompleteProps<
   T,
   Multiple extends boolean | undefined,
   DisableClearable extends boolean | undefined,
-  FreeSolo extends boolean | undefined
+  FreeSolo extends boolean | undefined,
 > extends AutocompleteProps<T, Multiple, DisableClearable, FreeSolo> {}
 
-// let DsAutocompleteDefaultPropsTypes: DsAutocompleteProps<boolean, undefined, undefined, undefined>
-
-export const DsAutocompleteDefaultProps: DsAutocompleteProps = {
-  Multiple: true,
-  DisableClearable: undefined,
-  FreeSolo: undefined,
-  ChipComponent: 'div'
-}
-
-const extendedProps: AutocompleteProps<Test extends {}> = {
-  Multiple: true,
-  DisableClearable: undefined,
-  FreeSolo: undefined,
-  ChipComponent: 'div'
-}
-
+export const DsAutocompleteDefaultProps = {}
