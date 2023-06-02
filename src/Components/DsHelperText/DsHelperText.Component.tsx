0337fa24fb81b4274a7fc5922f3625555ee8eb7d
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-02 16:09:23 +0530

    Fix DsHelperText

---
+++ src/Components/DsHelperText/DsHelperText.Component.tsx
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react'
+import {
+  DsHelperTextDefaultProps,
+  DsHelperTextProps
+} from './DsHelperText.Types'
+import { DsFormHelperText } from '../DsFormHelperText'
+import { DsBox } from '../DsBox'
+import { DsTypography } from '../DsTypography'
+import { DsRemixIcon } from '../DsRemixIcon'
+
+export default class DsHelperText extends PureComponent<DsHelperTextProps> {
+  static defaultProps = DsHelperTextDefaultProps
+
+  render() {
+    const { helperText, color, success, error, ...formHelperTextProps } =
+      this.props
+
+    const customColor = success ? 'success' : color
+
+    const iconClassName =
+      (error && 'ri-error-warning-fill') ||
+      (success && 'ri-checkbox-circle-fill') ||
+      false
+
+    return (
+      helperText && (
+        <DsFormHelperText
+          component="div"
+          color={customColor}
+          error={error}
+          {...formHelperTextProps}
+        >
+          <DsBox
+            sx={{
+              display: 'flex',
+              justifyContent: 'start',
+              alignItems: 'center',
+              color: `${customColor}.main`
+            }}
+          >
+            {iconClassName && (
+              <DsRemixIcon
+                className={iconClassName}
+                color="inherit"
+                fontSize="cool"
+                sx={{
+                  mr: 'var(--ds-spacing-quickFreeze)',
+                  padding: 'var(--ds-spacing-deepFreeze)'
+                }}
+              />
+            )}
+            <DsTypography
+              variant="bodyRegularSmall"
+              color="inherit"
+              sx={{ pl: 'var(--ds-spacing-deepFreeze)' }}
+            >
+              {helperText}
+              &#8203;
+            </DsTypography>
+          </DsBox>
+        </DsFormHelperText>
+      )
+    )
+  }
+}
