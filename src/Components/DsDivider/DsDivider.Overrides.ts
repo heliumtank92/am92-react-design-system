0ccc22fd8ab768b102d9fa88aaafdfb5b2d3548a
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-06-03 18:45:36 +0530

    DsDivider Fixed

--- src/Components/DsDivider/DsDivider.Overrides.ts
+++ src/Components/DsDivider/DsDivider.Overrides.ts
@@ -1,1 +1,29 @@
-export const DsDividerOverrides = {}
+import { DsDividerDefaultProps } from './DsDivider.Types'
+
+export const DsDividerOverrides = {
+  MuiDivider: {
+    defaultProps: DsDividerDefaultProps,
+    styleOverrides: {
+      root: {
+        borderColor: 'var(--ds-color-strokeDefault)'
+      },
+      light: {
+        borderColor: 'var(--ds-color-strokeDisabled)'
+      }
+    },
+    variants: [
+      {
+        props: { 'ds-size': 'M' },
+        style: {
+          borderWidth: '0.5px'
+        }
+      },
+      {
+        props: { 'ds-size': 'L' },
+        style: {
+          borderWidth: '6px'
+        }
+      }
+    ]
+  }
+}
--- src/Components/DsDivider/DsDivider.Types.ts
+++ src/Components/DsDivider/DsDivider.Types.ts
@@ -1,5 +1,9 @@
 import { DividerProps } from '@mui/material'
 
-export interface DsDividerProps extends DividerProps {}
+export interface DsDividerProps extends DividerProps {
+  'ds-size': 'M' | 'L'
+}
 
-export const DsDividerDefaultProps: DsDividerProps = {}
+export const DsDividerDefaultProps: DsDividerProps = {
+  'ds-size': 'M'
+}
