8d2c3efb49114a0b72a92fb25bce4b06efe10713
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-04 14:33:09 +0530

    Fix exports

--- src/Components/DsChip/DsChip.Component.tsx
+++ src/Components/DsChip/DsChip.Component.tsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import Chip from '@mui/material/Chip'
 import { DsChipProps, DsChipDefaultProps } from './DsChip.Types'
 
-export default class DsChip extends PureComponent<DsChipProps> {
+export class DsChip extends PureComponent<DsChipProps> {
   static defaultProps = DsChipDefaultProps
   render() {
     return <Chip {...this.props} ds-variant="chip" />
