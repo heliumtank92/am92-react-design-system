5aac9125fb44071016f4d9a2c666be1da7dbac1e
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-05 17:48:34 +0530

    Remove textTransform from MuiBadge CSS Overrides

--- src/Components/DsBadge/DsBadge.Overrides.ts
+++ src/Components/DsBadge/DsBadge.Overrides.ts
@@ -9,7 +9,6 @@ export const DsBadgeOverrides = {
         fontSize: 'var(--ds-typo-supportRegularInfo-fontSize)',
         lineHeight: 'var(--ds-typo-supportRegularInfo-lineHeight)',
         letterSpacing: 'var(--ds-typo-supportRegularInfo-letterSpacing)',
-        textTransform: 'var(--ds-typo-supportRegularInfo-textTransform)',
         padding: 'var(--ds-spacing-deepFreeze)',
         minWidth: '16px',
         height: '16px',
