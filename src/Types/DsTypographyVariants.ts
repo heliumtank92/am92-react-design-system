d51cb66e77522367ac09496c0367bd50dfa3f9d5
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-06-06 14:32:55 +0530

    Remove default typo variants

--- src/Types/DsTypographyVariants.ts
+++ src/Types/DsTypographyVariants.ts
@@ -58,4 +58,17 @@ export interface DsTypographyPropsVariantOverrides {
   supportRegularFootnote: true
   supportBoldTextButton: true
   supportRegularMetadata: true
+  h1: false
+  h2: false
+  h3: false
+  h4: false
+  h5: false
+  h6: false
+  subtitle1: false
+  subtitle2: false
+  body1: false
+  body2: false
+  button: false
+  caption: false
+  overline: false
 }
