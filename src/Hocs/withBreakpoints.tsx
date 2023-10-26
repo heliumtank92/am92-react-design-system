ea030973d47a35291b58babbe866ddb3e869e70d
Ankit Gandhi <ankitgandhi452@gmail.com>
2023-10-26 13:04:29 +0530

    Props exported for HOC withBreakpoints

--- src/Hocs/withBreakpoints.tsx
+++ src/Hocs/withBreakpoints.tsx
@@ -1,11 +1,16 @@
 import { Breakpoint, useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
+type BreakpointsMap = { [key in Breakpoint]?: boolean }
+export interface IwithBreakpoints {
+  breakpoints: BreakpointsMap
+}
+
 export function withBreakpoints(Child: React.ElementType) {
   return (props: any) => {
     const theme = useTheme()
     const { keys } = theme.breakpoints
-    let breakPoints: { [key in Breakpoint]?: boolean } = {}
+    let breakPoints: BreakpointsMap = {}
 
     keys.forEach((key: Breakpoint) => {
       breakPoints[key] = useMediaQuery(theme.breakpoints.only(key))
