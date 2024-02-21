e6ddff58a8d2ada4cb3b6d158b8243c4f7891ebe
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-02-22 00:35:02 +0530

    Fixed Component: DsTab styles

--- src/Components/DsTab/DsTab.Overrides.ts
+++ src/Components/DsTab/DsTab.Overrides.ts
@@ -1,6 +1,20 @@
 export const DsTabOverrides = {
   MuiTab: {
     defaultProps: {},
-    styleOverrides: {}
+    styleOverrides: {
+      root: {
+        fontWeight: 'var(--ds-typo-bodyRegularMedium-fontWeight)',
+        fontSize: 'var(--ds-typo-bodyRegularMedium-fontSize)',
+        lineHeight: 'var(--ds-typo-bodyRegularMedium-lineHeight)',
+        letterSpacing: 'var(--ds-typo-bodyRegularMedium-letterSpacing)',
+        padding: 'var(--ds-spacing-glacial)',
+        gap: 'var(--ds-spacing-glacial)',
+        minHeight: '36px',
+        '> .MuiTab-iconWrapper': {
+          fontSize: 'var(--ds-typo-fontSizeFrostbite)',
+          margin: 'var(--ds-spacing-zero)'
+        }
+      }
+    }
   }
 }
