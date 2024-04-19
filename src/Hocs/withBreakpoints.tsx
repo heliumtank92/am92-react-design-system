e2efa84ed1d19b2c06314a8d9c272be9b91b3448
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-04-19 19:07:33 +0530

    Added Feature: withBreakpoints HOC now returns breakpointValues too and added Typescript better support

--- src/Hocs/withBreakpoints.tsx
+++ src/Hocs/withBreakpoints.tsx
@@ -1,13 +1,32 @@
 import { Breakpoint, useTheme } from '@mui/material/styles'
 import useMediaQuery from '@mui/material/useMediaQuery'
+import { Subtract } from 'utility-types'
 
 type BreakpointsMap = { [key in Breakpoint]?: boolean }
+type BreakpointsValues = { [key in Breakpoint]?: number }
+
+/**
+ * Basic Interface to extend in components wrapping the below HOC.
+ *
+ * @export
+ * @interface IwithBreakpoints
+ */
 export interface IwithBreakpoints {
   breakpoints: BreakpointsMap
+  breakpointsValues: BreakpointsValues
 }
 
-export function withBreakpoints(Child: React.ElementType) {
-  return (props: any) => {
+/**
+ * HOC to provide breakpoints features
+ *
+ * @template P
+ * @param Child
+ * @returns
+ */
+export function withBreakpoints<P extends IwithBreakpoints>(
+  Child: React.ComponentType<P>
+) {
+  return (props: Subtract<P, IwithBreakpoints>): React.JSX.Element => {
     const theme = useTheme()
     const { keys } = theme.breakpoints
     let breakPoints: BreakpointsMap = {}
@@ -16,6 +35,12 @@ export function withBreakpoints(Child: React.ElementType) {
       breakPoints[key] = useMediaQuery(theme.breakpoints.only(key))
     })
 
-    return <Child {...props} breakpoints={breakPoints} />
+    return (
+      <Child
+        {...(props as P)}
+        breakpointsValues={theme.breakpoints}
+        breakpoints={breakPoints}
+      />
+    )
   }
 }
