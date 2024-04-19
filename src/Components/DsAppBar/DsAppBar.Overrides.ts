e7c452078066fe2d6358342bd97f128d138e6f74
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-04-20 02:53:41 +0530

    Fixed Component: DsAppBar According to the design System Style

--- src/Components/DsAppBar/DsAppBar.Overrides.ts
+++ src/Components/DsAppBar/DsAppBar.Overrides.ts
@@ -1,20 +1,22 @@
 import { DsAppBarDefaultProps } from './DsAppBar.Types'
 
 export const DsAppBarOverrides = {
-  defaultProps: DsAppBarDefaultProps,
-  styleOverrides: {
-    colorPrimary: {
-      backgroundColor: 'var(--ds-colour-actionPrimary)',
-      color: 'var(--ds-colour-typoOnSurface)',
-      '& .MuiIconButton-root': {
-        color: 'var(--ds-colour-iconOnSurface)'
-      }
-    },
-    colorDefault: {
-      backgroundColor: 'var(--ds-colour-surfaceBackground)',
-      color: 'var(--ds-colour-typoPrimary)',
-      '& .MuiIconButton-root': {
-        color: 'var(--ds-colour-iconDefault)'
+  MuiAppBar: {
+    defaultProps: DsAppBarDefaultProps,
+    styleOverrides: {
+      colorPrimary: {
+        backgroundColor: 'var(--ds-colour-actionPrimary)',
+        color: 'var(--ds-colour-typoOnSurface)',
+        '& .MuiIconButton-root': {
+          color: 'var(--ds-colour-iconOnSurface)'
+        }
+      },
+      colorDefault: {
+        backgroundColor: 'var(--ds-colour-surfaceBackground)',
+        color: 'var(--ds-colour-typoPrimary)',
+        '& .MuiIconButton-root': {
+          color: 'var(--ds-colour-iconDefault)'
+        }
       }
     }
   }
