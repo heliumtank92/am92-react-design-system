50b38d8f46902397ec96071f3a27b91d36a6785d
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-06-03 13:06:00 +0530

    Fixed Component: DsTabs Defaulting and border issuses

--- src/Components/DsTabs/DsTabs.Types.ts
+++ src/Components/DsTabs/DsTabs.Types.ts
@@ -4,4 +4,8 @@ export interface DsTabsProps extends TabsProps {
   'ds-variant'?: 'container'
 }
 
-export const DsTabsDefaultProps: DsTabsProps = {}
+export const DsTabsDefaultProps: DsTabsProps = {
+  variant: 'fullWidth',
+  indicatorColor: 'secondary',
+  textColor: 'secondary'
+}
