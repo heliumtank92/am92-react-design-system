1424154b6cae62af2f62a964568f292b99db9d33
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-06-26 01:34:59 +0530

    Fixed Types: DsTag Label changed to React.ReacNode from string

--- src/Components/DsTag/DsTag.Types.ts
+++ src/Components/DsTag/DsTag.Types.ts
@@ -2,13 +2,11 @@ import { ChipProps } from '@mui/material'
 
 export interface DsTagProps
   extends Omit<ChipProps, 'skipFocusWhenDisabled' | 'clickable' | 'color'> {
-  label: string
   value: any
   selected: boolean
 }
 
 export const DsTagDefaultProps: DsTagProps = {
-  label: '',
   value: undefined,
   selected: false
 }
