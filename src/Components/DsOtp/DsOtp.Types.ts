ded8189ba8debf87fb2e7eec2fdb802a7180190d
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-06-26 01:36:40 +0530

    Fixed Component: DsOtp reverted back

--- src/Components/DsOtp/DsOtp.Types.ts
+++ src/Components/DsOtp/DsOtp.Types.ts
@@ -1,27 +1,23 @@
 import { DsBoxProps } from '..'
 import { DsTextFieldProps } from '../DsTextField'
 
-export type T_VALUE = string
 export interface DsOtpProps extends DsTextFieldProps {
-  onComplete: (value: T_VALUE) => void
+  onComplete: (otpString: string) => void
   name?: string
   length?: number
-  initialOtp?: T_VALUE
+  initialOtp?: string
   BoxProps?: DsBoxProps
-  value?: T_VALUE
-  autoFocus?: boolean
 }
 
 export const DsOtpDefaultProps: DsOtpProps = {
   name: 'otp',
   length: 6,
-  onComplete: () => undefined,
-  autoFocus: false
+  onComplete: () => undefined
 }
 export interface DsOtpState {
   otp: string[]
 }
 
-export const DsOtpDefaultState: DsOtpState = {
+export const DsOtpDefaultState = {
   otp: []
 }
