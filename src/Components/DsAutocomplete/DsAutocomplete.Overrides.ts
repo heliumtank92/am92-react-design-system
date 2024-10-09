8a84d49a23e7cb508d485a9376cefb3c2e519ede
Ankit Gandhi <ankitgandhi452@gmail.com>
2024-10-09 15:26:06 +0530

    Fixed Component: DsAutocomplete & DsSearchbar Css Fixes

--- src/Components/DsAutocomplete/DsAutocomplete.Overrides.ts
+++ src/Components/DsAutocomplete/DsAutocomplete.Overrides.ts
@@ -25,7 +25,6 @@ export const DsAutocompleteOverrides = {
         }
       },
       endAdornment: {
-        top: 'unset',
         right: '8px',
         display: 'flex',
         alignItems: 'center',
